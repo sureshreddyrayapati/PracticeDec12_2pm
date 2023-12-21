ac4ee13c091090e4e794678afd8d67abc5afe205
sureshreddyrayapati <resuresh0@gmail.com>
2023-12-21 23:00:58 +0530

    Add project files.

---
+++ exceptionHandling/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class A
+{
+    static A x = new A();
+    A() {
+        
+        Console.WriteLine("This is Default Constuctor");
+    } 
+    A(int x)
+    {
+        Console.WriteLine(x);
+        Console.WriteLine("This is parameterised Constructor");
+    }
+    void M1()
+    {
+        Console.WriteLine(x.M2(Convert.ToInt32(Console.ReadLine())));
+        Console.WriteLine("This is method 1");
+    }
+    int M2(int a)
+    {
+
+        Console.WriteLine("This is method 2");
+        Console.WriteLine(M3(Console.ReadLine()));
+        return a * a;
+    }
+    static String M3(String a)
+    {
+        return a;
+    }
+    public static void Main(String[] args)
+    {
+        A x = new A();
+        new A(4);
+        x.M1();
+    }
+}
---
+++ exceptionHandling/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionHandling
+{
+    class MyError:ApplicationException
+    {
+        public void waterOverFlowException()
+        {
+            Console.WriteLine("Exception Happend tank is full");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int number = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(Suresh(number));
+            // String word=Console.ReadLine();
+            // Console.WriteLine(recString(word));
+            try
+            {
+                Console.WriteLine("Fill the can of water in liters\n How meny liters");
+                int water = Convert.ToInt32(Console.ReadLine());
+                if(water>=100)
+                {
+                    throw new MyError(); 
+                }
+                else
+                {
+                    Console.WriteLine($"Tank is filled with {water} liters");
+                }
+                
+            }
+            catch (MyError e)
+            {
+                e.waterOverFlowException();
+            }
+        }
+        public static int Suresh(int a)
+        {
+            int rev = 0;
+            while(a>0)
+            {
+                int x = a % 10;
+                rev = rev * 10 + x;
+                a = a / 10;
+            }
+            return rev;
+        }
+        public static string recString(string w)
+        {
+            String d = "";
+            for(int i=w.Length-1;i>=0;i--)
+            {
+                char s = w.ToCharArray()[i];
+                d = d + s;
+            }
+            return d;   
+        }
+
+    }
+    
+}
